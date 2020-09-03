7830149c2100c4cbbb6774a9a89587eac28b2666
Bartek <shox12.47@gmail.com>
2020-09-03 14:55:20 +0200

    add user data fetching in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyledSection } from './Home.styles';
+import axios from 'axios';
 
 const Home = () => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios('https://reqres.in/api/users?page=2');
+        setData(result.data.data);
+      } catch {
+
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div>Home Page</div>
+    <StyledSection>
+      <ul>
+        {
+          data &&
+          data.map(user => (
+            <li key={user.id}>{user.first_name} {user.last_name}</li>
+          ))
+        }
+      </ul>
+    </StyledSection>
   );
 }
- 
+
 export default Home;
