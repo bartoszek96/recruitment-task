7456c8c0a832deefa820abb70d9fc1f73dddffaf
Bartek <shox12.47@gmail.com>
2020-09-06 15:53:31 +0200

    Updates

---
+++ precache-manifest.e6fb96ca245e7cbf066ed19d296f46f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f97cd0475808e2557a5fc655a5de3a",
+    "url": "/recruitment-task/index.html"
+  },
+  {
+    "revision": "aca5175b27487ed6da98",
+    "url": "/recruitment-task/static/js/2.438360a3.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/recruitment-task/static/js/2.438360a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751489d0ecd0ecdaf741",
+    "url": "/recruitment-task/static/js/main.025d0636.chunk.js"
+  },
+  {
+    "revision": "0f3ff79579a134f7b92c",
+    "url": "/recruitment-task/static/js/runtime-main.51ddc63e.js"
+  }
+]);
