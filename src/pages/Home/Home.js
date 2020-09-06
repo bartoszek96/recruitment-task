96af433a38d7de766661176b7273ae32e9cda07c
Bartek <shox12.47@gmail.com>
2020-09-06 13:22:16 +0200

    data fetching without axios

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,45 +7,45 @@ import {
 } from './Home.styles';
 import Button from '@material-ui/core/Button';
 import ReactLoading from "react-loading";
-import axios from 'axios';
 
 const Home = ({ handleShowMenuTextButtonClick }) => {
 
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchUsersData = async () => {
       try {
-        setError(false);
-        const result = await axios('https://reqres.in/api/users?page=2');
-        if (result.status !== 200) {
-          throw new Error();
-        } else {
-          setData(result.data.data);
-          setIsLoading(false);
-        }
+        setError('');
+        const result = await fetch('https://reqres.in/api/users?page=2');
+        const json = await result.json();
+        setData(json);
+        setIsLoading(false);
       } catch {
-        setError(true);
+        setError('Data fetching error!');
         setIsLoading(false);
       }
     }
     setIsLoading(true);
-    fetchData();
+    fetchUsersData();
   }, [])
 
   return (
     <StyledSection>
-      <Button variant="contained" color="primary" onClick={handleShowMenuTextButtonClick}>POKAŻ</Button>
-      {error && <StyledErrorSection>Data fetching error!</StyledErrorSection>}
+      <Button variant="contained" color="primary" onClick={handleShowMenuTextButtonClick}>
+        POKAŻ
+      </Button>
+      <StyledErrorSection>
+        {error}
+      </StyledErrorSection>
       <StyledLoaderSection>
-        {isLoading && <ReactLoading type='spin' color='black' />}
+        {isLoading && <ReactLoading type='spin' color='black' data-testid='loader' />}
       </StyledLoaderSection>
       <StyledList>
         {
           data &&
-          data.map(user => (
+          data.data.map(user => (
             <li key={user.id}>{user.first_name} {user.last_name}</li>
           ))
         }
